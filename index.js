7f7a4a7416aea1c1dff0a47d76c5962347a53dff
Sunand <sunandtwork@gmail.com>
2025-12-16 11:15:42 +0530

    start

---
+++ index.js
@@ -0,0 +1,31 @@
+
+        AOS.init({
+            duration: 1000,
+            once: true
+        });
+
+        document.getElementById('contactForm').addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Thank you for your message! We will get back to you soon.');
+            this.reset();
+        });
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+                }
+            });
+        });
+
+
+
+
+        document.querySelectorAll(".blog-link").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    alert("Blog detail page coming soon!");
+  });
+});
